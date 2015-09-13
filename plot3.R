3dd2971a15d2c24d5e661179bfcde8e32ad01185
kale-male <e.khripunova@gmail.com>
2015-09-13 18:40:32 +0300

    Put function description into plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,35 @@
 source("lab.R")
 library(datasets)
 
+plot3 <- function(bty = "y"){
+  
+  with(Elpowcons, {
+    plot(TimeData,
+         Sub_metering_1, 
+         type = "n", 
+         xlab = "", 
+         ylab = "Energy sub metering")
+    
+    points(TimeData,
+           Sub_metering_1, 
+           type = "l", 
+           col = "black")
+    points(TimeData,
+           Sub_metering_2, 
+           type = "l", 
+           col = "red")
+    points(TimeData,
+           Sub_metering_3,
+           type = "l", 
+           col = "blue")
+  })
+  legend("topright", pch = "-", 
+         col = c("black", "red", "blue"), 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+         bty = bty)
+  
+}
+
 png(filename = "plot3.png")
 plot3()
 dev.off()
